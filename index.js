14c12092a527a7f1c55873977577406b15b8df09
jamilaahme98 <jamilaahmada1998@gmail.com>
2022-10-04 23:11:16 +0100

    about

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 var num1 = document.getElementById("counter1").innerHTML;
 
 for(let i=0; i<=num1; i++){
     document.getElementById("counter1").innerHTML =i;
 }
 
-=======
->>>>>>> 6aebbe0313df16992be9abfa3d35262f74dd50c4
